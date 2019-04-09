002dc24956ec6db16b2112ced24e70b0199af559
Unknown <jonas.de.ridder@student.howest.be>
2019-04-09 11:05:56 +0200

    create project

---
+++ app/src/main/java/be/nextapps/jonas/bap_todo_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package be.nextapps.jonas.bap_todo_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
